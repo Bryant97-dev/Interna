826efa31ff3dae76d1bb7cb9e3c5e685ccc1c678
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-03 22:24:47 +0700

    administrative data create done

--- app/Http/Controllers/AdministrativeDataController.php
+++ app/Http/Controllers/AdministrativeDataController.php
@@ -55,40 +55,13 @@ class AdministrativeDataController extends Controller
 
         AdministrativeData::create([
             'title' => $data['title'],
-            'file' => $path,
+            'file' => $name,
+            'path' => $path,
             'description' => $data['description'],
             'user_id' => $data['user_id'],
         ]);
 
-        return $path;
-
-        // menyimpan data file yang diupload ke variabel $file
-//        $file = $request->file('file');
-
-        // nama file
-//        echo 'File Name: '.$file->getClientOriginalName();
-//        echo '<br>';
-//
-//        // ekstensi file
-//        echo 'File Extension: '.$file->getClientOriginalExtension();
-//        echo '<br>';
-//
-//        // real path
-//        echo 'File Real Path: '.$file->getRealPath();
-//        echo '<br>';
-//
-//        // ukuran file
-//        echo 'File Size: '.$file->getSize();
-//        echo '<br>';
-//
-//        // tipe mime
-//        echo 'File Mime Type: '.$file->getMimeType();
-
-        // isi dengan nama folder tempat kemana file diupload
-//        $tujuan_upload = 'data_file';
-
-        // upload file
-//        $file->move($tujuan_upload,$file->getClientOriginalName());
+        return redirect()->route('administrative-data.index');
     }
 
     /**
--- resources/views/administrative_data/index.blade.php
+++ resources/views/administrative_data/index.blade.php
@@ -21,7 +21,7 @@
                                         Title
                                     </th>
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        File
+                                        File Name
                                     </th>
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Description
