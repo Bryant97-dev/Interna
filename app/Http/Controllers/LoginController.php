2798bad660d627a52ba5b75c727b0182a74ddec9
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-05 01:20:51 +0700

    api login logout controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(! $user->tokens() == null){
+            $user->tokens()->delete();
+        }
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response([
+                'status' => 404,
+                'message' => "Not Found!!!"
+            ]);
+        }
+        return response([
+            'access_token'=>$user->createToken('token')->plainTextToken,
+            'status'=>'200',
+            'message'=>'Success'
+        ]);
+    }
+
+    public function logout(Request $request){
+        auth()->logout();
+        return response()->json([
+            'message'    => "Success"
+        ], 200);
+
+//        $user = User::where('email', $request->email)->first();
+//        Auth::guard('web')->logout();
+//        $user->tokens()->delete();
+//        return response()->json([
+//            'success'    => true
+//        ], 200);
+    }
 }
