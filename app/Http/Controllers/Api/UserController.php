bb7b2045af792b2674b0261c20628531ca178c26
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-10 01:52:41 +0700

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function user (){
-        $users = Auth::user()->where('id', '=', Auth::id())->get();
+        $users = User::where('id', '=', Auth::id())->get();
         return response()->json($users);
     }
 }
