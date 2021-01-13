80668db9c139bb76eb43b338b4243e3dbb5c0ea5
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-13 20:11:45 +0700

    api user fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,24 +10,26 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     public function user (){
-        $users = Auth::user();
-        if (empty($users->profile_photo_path))
-        {
-            $users->profile_photo_path = $users->profile_photo_url;
-        }
-        return response([
-            'id'                    => $users->id,
-            'study_program'         => $users->study_programs->abbreviation,
-            'period'                => $users->periods->period,
-            'name'                  => $users->name,
-            'email'                 => $users->email,
-            'nim'                   => $users->nim,
-            'gender'                => $users->gender,
-            'line_account'          => $users->line_account,
-            'phone'                 => $users->phone,
-            'batch'                 => $users->batch,
-            'description'           => $users->description,
-            'profile_photo_path'    => $users->profile_photo_path,
-        ]);
+        $users = User::with('study_programs')->with('periods')->where('id', '=', Auth::id())->get();
+        return response($users);
+//        $users = Auth::user();
+//        if (empty($users->profile_photo_path))
+//        {
+//            $users->profile_photo_path = $users->profile_photo_url;
+//        }
+//        return response([
+//            'id'                    => $users->id,
+//            'study_program'         => $users->study_programs->abbreviation,
+//            'period'                => $users->periods->period,
+//            'name'                  => $users->name,
+//            'email'                 => $users->email,
+//            'nim'                   => $users->nim,
+//            'gender'                => $users->gender,
+//            'line_account'          => $users->line_account,
+//            'phone'                 => $users->phone,
+//            'batch'                 => $users->batch,
+//            'description'           => $users->description,
+//            'profile_photo_path'    => $users->profile_photo_path,
+//        ]);
     }
 }
