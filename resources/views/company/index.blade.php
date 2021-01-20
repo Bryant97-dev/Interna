6da9eaf15a9c45f3f6d2bf2dda4f5fa68f020a24
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-20 20:25:24 +0700

    user company fix

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -9,36 +9,30 @@
                     <div class="py-12 bg-white">
                         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                             <div class="lg:text-center">
+                                @if(empty($status))
+                                    <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                                        Status : Pending
+                                    </p>
+                                @else
+                                    @if($status == 1)
+                                        <p class="mt-4 max-w-2xl text-xl text-green-500 lg:mx-auto">
+                                            Status : Approved
+                                        </p>
+                                    @else
+                                        <p class="mt-4 max-w-2xl text-xl text-red-500 lg:mx-auto">
+                                            Status : Rejected
+                                        </p>
+                                    @endif
+                                @endif
                                 <h2 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl">
                                     {{ $name ?: 'There is no Company Name' }}
                                 </h2>
                                 <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
                                     {{ $address ?: 'There is no Company Address' }}
                                 </p>
-                                <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                                    {{ $company_phone ?: 'There is no Company Phone' }}
-                                </p>
-                                <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                                    {{ $siup ?: 'There is no Company SIUP' }}
-                                </p>
-                                @if(empty($status))
-                                    <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                                        Pending
-                                    </p>
-                                @else
-                                    @if($status == 1)
-                                        <p class="mt-4 max-w-2xl text-xl text-green-500 lg:mx-auto">
-                                            Approved
-                                        </p>
-                                    @else
-                                        <p class="mt-4 max-w-2xl text-xl text-red-500 lg:mx-auto">
-                                            Rejected
-                                        </p>
-                                    @endif
-                                @endif
                             </div>
 
-                            <div class="mt-10">
+                            <div class="mt-10 pt-8">
                                 <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
                                     <div class="flex">
                                         <div class="flex-shrink-0">
@@ -54,6 +48,42 @@
                                             </dt>
                                             <dd class="mt-2 text-base text-gray-500">
                                                 {{ $supervisor ?: 'There is no Supervisor Name' }}
+                                            </dd>
+                                        </div>
+                                    </div>
+
+                                    <div class="flex">
+                                        <div class="flex-shrink-0">
+                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div class="ml-4">
+                                            <dt class="text-lg leading-6 font-medium text-gray-900">
+                                                Supervisor Phone
+                                            </dt>
+                                            <dd class="mt-2 text-base text-gray-500">
+                                                {{ $supervisor_phone ?: 'There is no Supervisor Phone' }}
+                                            </dd>
+                                        </div>
+                                    </div>
+
+                                    <div class="flex">
+                                        <div class="flex-shrink-0">
+                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div class="ml-4">
+                                            <dt class="text-lg leading-6 font-medium text-gray-900">
+                                                Company Phone
+                                            </dt>
+                                            <dd class="mt-2 text-base text-gray-500">
+                                                {{ $company_phone ?: 'There is no Company Phone' }}
                                             </dd>
                                         </div>
                                     </div>
@@ -81,16 +111,16 @@
                                         <div class="flex-shrink-0">
                                             <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                                                 <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                                    <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
                                                 </svg>
                                             </div>
                                         </div>
                                         <div class="ml-4">
                                             <dt class="text-lg leading-6 font-medium text-gray-900">
-                                                Supervisor Phone
+                                                Company NPWP
                                             </dt>
                                             <dd class="mt-2 text-base text-gray-500">
-                                                {{ $supervisor_phone ?: 'There is no Supervisor Phone' }}
+                                                {{ $npwp ?: 'There is no Company NPWP' }}
                                             </dd>
                                         </div>
                                     </div>
@@ -105,10 +135,10 @@
                                         </div>
                                         <div class="ml-4">
                                             <dt class="text-lg leading-6 font-medium text-gray-900">
-                                                Company NPWP
+                                                Company SIUP
                                             </dt>
                                             <dd class="mt-2 text-base text-gray-500">
-                                                {{ $npwp ?: 'There is no Company NPWP' }}
+                                                {{ $siup ?: 'There is no Company SIUP' }}
                                             </dd>
                                         </div>
                                     </div>
