dfc4f04f87fb036d61bee848943a2e333a09a484
Franciscus Valentinus Ongkosianbhadra <74890602+franciscusvalentinus@users.noreply.github.com>
2021-01-21 17:22:26 +0700

    admin user fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -31,7 +31,7 @@
                                             <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Roles
                                             </th>
-                                            <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                            <th scope="col" width="100" class="px-6 py-3 bg-gray-50">
 
                                             </th>
                                         </tr>
